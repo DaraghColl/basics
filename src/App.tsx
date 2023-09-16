431179ee970e1a31c746bac090a942a3f43f2d1a
Daragh <daraghcoll-@hotmail.com>
2023-09-16 15:49:25 +0700

    feat: header with theme toggle and home link

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,19 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { TopicsList } from './pages/topics-list/topics-list';
 import { TopicLearn } from './pages/topic-learn/topic-learn';
+import { Header } from './components/header/header';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<TopicsList />} />
-        <Route path="/topic-learn" element={<TopicLearn />} />
-      </Routes>
-    </Router>
+    <div className="container">
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<TopicsList />} />
+          <Route path="/topic-learn" element={<TopicLearn />} />
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
