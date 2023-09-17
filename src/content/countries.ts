1eb1bd6fdbaabcbe65efe0a6e4f302b97fa22bd2
Daragh <daraghcoll-@hotmail.com>
2023-09-17 23:43:23 +0700

    feat: add countries and flags topics

---
+++ src/content/countries.ts
@@ -0,0 +1,242 @@
+import { TopicData } from '../types/types';
+
+const europeanCuntriesData: TopicData[] = [
+  {
+    front: '🇮🇪',
+    back: 'Ireland',
+  },
+  {
+    front: '🇮🇹',
+    back: 'Italy',
+  },
+  {
+    front: '🇩🇪',
+    back: 'Germany',
+  },
+  {
+    front: '🏴󠁧󠁢󠁥󠁮󠁧󠁿',
+    back: 'England',
+  },
+  {
+    front: '🏴󠁧󠁢󠁳󠁣󠁴󠁿',
+    back: 'Scotland',
+  },
+  {
+    front: '🏴󠁧󠁢󠁷󠁬󠁳󠁿',
+    back: 'Wales',
+  },
+  {
+    front: '🇫🇷',
+    back: 'France',
+  },
+  {
+    front: '🇳🇱',
+    back: 'Netherlands',
+  },
+  {
+    front: '🇨🇭',
+    back: 'Switzerland',
+  },
+  {
+    front: '🇬🇷',
+    back: 'Greece',
+  },
+  {
+    front: '🇵🇱',
+    back: 'Poland',
+  },
+  {
+    front: '🇺🇦',
+    back: 'Ukraine',
+  },
+  {
+    front: '',
+    back: 'Austria',
+  },
+  {
+    front: '🇧🇪',
+    back: 'Belgium',
+  },
+  {
+    front: '🇸🇪',
+    back: 'Sweden',
+  },
+  {
+    front: '🇭🇷',
+    back: 'Croatia',
+  },
+  {
+    front: '🇳🇴',
+    back: 'Norway',
+  },
+  {
+    front: '🇩🇰',
+    back: 'Denmark',
+  },
+  {
+    front: '🇦🇱',
+    back: 'Albania',
+  },
+  {
+    front: '🇷🇴',
+    back: 'Ramania',
+  },
+  {
+    front: '🇲🇹',
+    back: 'Malta',
+  },
+  {
+    front: '🇮🇸',
+    back: 'Iceland',
+  },
+  {
+    front: '🇫🇮',
+    back: 'Finland',
+  },
+  {
+    front: '🇷🇸',
+    back: 'Serbia',
+  },
+  {
+    front: '🇨🇿',
+    back: 'Czechia',
+  },
+  {
+    front: '🇭🇺',
+    back: 'Hungary',
+  },
+  {
+    front: '🇸🇮',
+    back: 'Slovenia',
+  },
+  {
+    front: '🇧🇬',
+    back: 'Bulgaria',
+  },
+  {
+    front: '🇸🇮',
+    back: 'Slovenia',
+  },
+  {
+    front: '🇱🇺',
+    back: 'Luxembourg',
+  },
+  {
+    front: '🇨🇾',
+    back: 'Cyprus',
+  },
+  {
+    front: '🇲🇪',
+    back: 'Montenegro',
+  },
+  {
+    front: '🇪🇪',
+    back: 'Estonia',
+  },
+  {
+    front: '🇱🇹',
+    back: 'Lithuania',
+  },
+  {
+    front: '🇽🇰',
+    back: 'Kosovo',
+  },
+  {
+    front: '🇸🇰',
+    back: 'Slovakia',
+  },
+  {
+    front: '🇱🇻',
+    back: 'Latvia',
+  },
+  {
+    front: '🇱🇮',
+    back: 'Liechtenstein',
+  },
+  {
+    front: '🇧🇾',
+    back: 'Belarus',
+  },
+  {
+    front: '🇧🇦',
+    back: 'Bosnia and Herzegovina',
+  },
+  {
+    front: '🇲🇰',
+    back: 'North Macedonia',
+  },
+  {
+    front: '🇻🇦',
+    back: 'Vatican City',
+  },
+  {
+    front: '🇲🇨',
+    back: 'Monaco',
+  },
+  {
+    front: '🇲🇩',
+    back: 'Moldova',
+  },
+  {
+    front: '🇦🇩',
+    back: 'Andorra',
+  },
+  {
+    front: '🇮🇲',
+    back: 'Isle of Man',
+  },
+  {
+    front: '🇬🇮',
+    back: 'Gibraltar',
+  },
+  {
+    front: '🇯🇪',
+    back: 'Jersey',
+  },
+  {
+    front: '🇸🇲',
+    back: 'San Marino',
+  },
+  {
+    front: '🇦🇽',
+    back: 'Åland Islands',
+  },
+  {
+    front: '🇫🇴',
+    back: 'Faroe Islands',
+  },
+  {
+    front: '🇬🇮',
+    back: 'Gibraltar',
+  },
+  {
+    front: '🇬🇮',
+    back: 'Gibraltar',
+  },
+  {
+    front: '🇬🇮',
+    back: 'Gibraltar',
+  },
+  {
+    front: '🇬🇮',
+    back: 'Gibraltar',
+  },
+  {
+    front: '🇬🇮',
+    back: 'Gibraltar',
+  },
+  {
+    front: '🇬🇮',
+    back: 'Gibraltar',
+  },
+  {
+    front: '🇬🇮',
+    back: 'Gibraltar',
+  },
+  {
+    front: '🇬🇮',
+    back: 'Gibraltar',
+  },
+];
+
+export { europeanCuntriesData };
--- src/content/topics.ts
+++ src/content/topics.ts
@@ -3,34 +3,43 @@ import { irishData } from './language/irish';
 import { timesTablesToTenMathData } from './math/math';
 import { animalData } from './animals';
 import { Topic } from '../types/types';
+import { europeanCuntriesData } from './countries';
 
 const topicList: Topic[] = [
   {
     id: 0,
     title: 'Alphabet',
     description: 'Learn the aplhabet with word association and images 🍎',
-    icon: 'language',
+    icon: 'learning',
     data: alphabetData,
   },
   {
     id: 1,
     title: 'Times Tables 1-10',
-    description: 'Learn and test yourself with times tables up to 10',
+    description: 'Learn and test yourself with times tables up to 10 🔢',
     icon: 'math',
     data: timesTablesToTenMathData,
     canAnswer: true,
   },
-
   {
     id: 2,
-    title: 'Animals',
-    description: 'Learn all the animals names with pictures 🙂',
-    icon: 'language',
-    data: animalData,
+    title: 'Eurpean Countries & Flags',
+    description: 'Learn and test yourself on the european countries and their flags 🇮🇪',
+    icon: 'country',
+    data: europeanCuntriesData,
+    canAnswer: true,
     frontCardLarge: true,
   },
   {
     id: 3,
+    title: 'Animals',
+    description: 'Learn all the animals names with pictures 🙂',
+    icon: 'country',
+    data: animalData,
+    frontCardLarge: true,
+  },
+  {
+    id: 4,
     title: 'Irish',
     description: 'Learn the basics of the irish language with common phrases and sayings 🇮🇪',
     icon: 'language',
