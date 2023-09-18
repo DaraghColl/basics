2b85a61436de696a35d36a8d9b59d0b2ff6158e3
Daragh <daraghcoll-@hotmail.com>
2023-09-19 01:33:15 +0700

    feat: replace header and theme toggle styles with tailwind

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,13 +1,12 @@
 import { FC } from 'react';
-import './header.css';
 import { Link } from 'react-router-dom';
 import { ThemeToggle } from '../theme-toggle/theme-toggle';
 
 const Header: FC = () => {
   return (
-    <header className="header">
+    <header className="mx-auto my-0 flex max-w-[800px] items-center justify-between px-0 py-8">
       <Link to="/" id="home_icon_link" aria-label="link to home page">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <svg className="w-10" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
           <path d="M16.5 8.25V6a2.25 2.25 0 00-2.25-2.25H6A2.25 2.25 0 003.75 6v8.25A2.25 2.25 0 006 16.5h2.25m8.25-8.25H18a2.25 2.25 0 012.25 2.25V18A2.25 2.25 0 0118 20.25h-7.5A2.25 2.25 0 018.25 18v-1.5m8.25-8.25h-6a2.25 2.25 0 00-2.25 2.25v6" />
         </svg>
       </Link>
