6fd85d46b123749841c0f7541a16d1b5adfeb5bc
Daragh <daraghcoll-@hotmail.com>
2023-09-19 01:09:29 +0700

    feat: custom primary secondary tailwind color

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          light: '#f1f5f9',
+          DEFAULT: '#f1f5f9',
+          dark: '#131517',
+        },
+        secondary: {
+          light: '#ffffff',
+          DEFAULT: '#ffffff',
+          dark: '#1c1e23',
+        },
+      },
+    },
   },
   plugins: [],
   darkMode: 'class',
