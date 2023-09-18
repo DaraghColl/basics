fd863f67a9a144a99016060f6867cbca015c2227
Daragh <daraghcoll-@hotmail.com>
2023-09-19 00:01:16 +0700

    feat: change topic list class to tailwind

--- src/pages/topic-list/topic-list.tsx
+++ src/pages/topic-list/topic-list.tsx
@@ -5,7 +5,7 @@ import { TopicItem } from './topic-item';
 
 const TopicList: FC = () => {
   return (
-    <div className="topic-list">
+    <div className="grid grid-cols-1 gap-8 pb-8 md:grid-cols-2">
       {topicList.map((topic) => {
         return <TopicItem topic={topic} key={topic.id} />;
       })}
